3c196be93a6083710bb2536017e562a8ad3411c3
André Fonseca <andre.j.fonseca@gmail.com>
2021-02-20 18:05:54 +0000

    adding more pieces
    Adding bishop and rook pieces

--- Chess_game/JChess/src/com/chess/engine/Pieces/Knight.java
+++ Chess_game/JChess/src/com/chess/engine/Pieces/Knight.java
@@ -13,9 +13,9 @@ import com.chess.engine.board.Tile;
 
 public class Knight extends Piece {
 
-	private final static int[] CANDIDATE_MOVE_COORDINATES = {-17,-15,-10,-6,6,10,15,17};
-	
-	
+	private final static int[] CANDIDATE_MOVE_COORDINATES = { -17, -15, -10,
+			-6, 6, 10, 15, 17 };
+
 	Knight(int piecePosition, Alliance pieceAlliance) {
 		super(piecePosition, pieceAlliance);
 		// TODO Auto-generated constructor stub
@@ -24,60 +24,80 @@ public class Knight extends Piece {
 	@Override
 	public Collection<Move> calculateLegalMoves(Board board) {
 		// TODO Auto-generated method stub
-		
+
 		int candidateDestinationCoordinate;
 		final List<Move> legalMoves = new ArrayList<>();
-		
-		for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES){
-			
-			candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
-			
-			if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
-				
-				if(isFirstColumnExclusion(this.piecePosition, currentCandidateOffset)
-				|| isSecondColumnExclusion(this.piecePosition, currentCandidateOffset) 
-				|| isSeventhColumnExclusion(this.piecePosition, currentCandidateOffset)
-				|| isEightColumnExclusion(this.piecePosition, currentCandidateOffset)){
+
+		for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES) {
+
+			candidateDestinationCoordinate = this.piecePosition
+					+ currentCandidateOffset;
+
+			if (BoardUtils
+					.isValidTileCoordinate(candidateDestinationCoordinate)) {
+
+				if (isFirstColumnExclusion(this.piecePosition,
+						currentCandidateOffset)
+						|| isSecondColumnExclusion(this.piecePosition,
+								currentCandidateOffset)
+						|| isSeventhColumnExclusion(this.piecePosition,
+								currentCandidateOffset)
+						|| isEightColumnExclusion(this.piecePosition,
+								currentCandidateOffset)) {
 					continue;
-					}
-				
-				final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
-				
-				if(!candidateDestinationTile.isTileOccupied()){
-					legalMoves.add(new Move.MajorMove(board,this,candidateDestinationCoordinate));
-				}else{
-					final Piece pieceAtDestination = candidateDestinationTile.getPiece();
-					final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
-					
-					
-					if(this.pieceAlliance != pieceAlliance){
-						legalMoves.add(new Move.AttackMove(board,this,candidateDestinationCoordinate,pieceAtDestination));
-					}
-					
 				}
-				
+
+				final Tile candidateDestinationTile = board
+						.getTile(candidateDestinationCoordinate);
+
+				if (!candidateDestinationTile.isTileOccupied()) {
+					legalMoves.add(new Move.MajorMove(board, this,
+							candidateDestinationCoordinate));
+				} else {
+					final Piece pieceAtDestination = candidateDestinationTile
+							.getPiece();
+					final Alliance pieceAlliance = pieceAtDestination
+							.getPieceAlliance();
+
+					if (this.pieceAlliance != pieceAlliance) {
+						legalMoves.add(new Move.AttackMove(board, this,
+								candidateDestinationCoordinate,
+								pieceAtDestination));
+					}
+
+				}
+
 			}
-			
+
 		}
-		
-		//ImmutableList
+
+		// ImmutableList
 		return Collections.unmodifiableList(legalMoves);
 	}
 
-	private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset){
-		return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -17 || candidateOffset == -10
-				|| candidateOffset == 6 || candidateOffset == 15);	
+	private static boolean isFirstColumnExclusion(final int currentPosition,
+			final int candidateOffset) {
+		return BoardUtils.FIRST_COLUMN[currentPosition]
+				&& (candidateOffset == -17 || candidateOffset == -10
+						|| candidateOffset == 6 || candidateOffset == 15);
 	}
 
-	private static boolean isSecondColumnExclusion(final int currentPosition, final int candidateOffset){
-		return BoardUtils.SECOND_COLUMN[currentPosition] && (candidateOffset == -10 || candidateOffset == 6);	
+	private static boolean isSecondColumnExclusion(final int currentPosition,
+			final int candidateOffset) {
+		return BoardUtils.SECOND_COLUMN[currentPosition]
+				&& (candidateOffset == -10 || candidateOffset == 6);
 	}
-	
-	private static boolean isSeventhColumnExclusion(final int currentPosition, final int candidateOffset){
-		return BoardUtils.SEVENTH_COLUMN[currentPosition] && (candidateOffset == -6 || candidateOffset == 10);	
+
+	private static boolean isSeventhColumnExclusion(final int currentPosition,
+			final int candidateOffset) {
+		return BoardUtils.SEVENTH_COLUMN[currentPosition]
+				&& (candidateOffset == -6 || candidateOffset == 10);
 	}
-	private static boolean isEightColumnExclusion(final int currentPosition, final int candidateOffset){
-		return BoardUtils.EIGHT_COLUMN[currentPosition] && (candidateOffset == -15 || candidateOffset == -6 
-				||candidateOffset == 10 || candidateOffset ==17);	
+
+	private static boolean isEightColumnExclusion(final int currentPosition,
+			final int candidateOffset) {
+		return BoardUtils.EIGHT_COLUMN[currentPosition]
+				&& (candidateOffset == -15 || candidateOffset == -6
+						|| candidateOffset == 10 || candidateOffset == 17);
 	}
 }
--- Chess_game/JChess/src/com/chess/engine/board/Tile.java
+++ Chess_game/JChess/src/com/chess/engine/board/Tile.java
@@ -9,66 +9,67 @@ import com.chess.engine.Pieces.Piece;
 public abstract class Tile {
 
 	protected final int tileCoordinate;
-	
-	private static final Map<Integer,EmptyTile> EMPTY_TILES_CACHE = creatAllPossibleEmptyTiles();
-	
-	private Tile(int tileCoordinate){
-		this.tileCoordinate=tileCoordinate;
-	}
-	
-	private static Map<Integer, EmptyTile> creatAllPossibleEmptyTiles() {
-		
-		final Map<Integer,EmptyTile> emptyTileMap = new HashMap<>();
-		
-		for(int i=0; i<BoardUtils.NUM_TILES;i++ ){
-			emptyTileMap.put(i, new EmptyTile(i));
-		}
-		
-		//return ImmutableMap.copyOf(emptyTileMap);
-		return Collections.unmodifiableMap(emptyTileMap);
-		//return emptyTileMap;
+
+	private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = creatAllPossibleEmptyTiles();
+
+	private Tile(int tileCoordinate) {
+		this.tileCoordinate = tileCoordinate;
 	}
 
-	public static Tile createTile(final int tileCoordinate, final Piece piece){
-		return piece !=null ? new OccupiedTile(tileCoordinate, piece):EMPTY_TILES_CACHE.get(tileCoordinate); 
+	private static Map<Integer, EmptyTile> creatAllPossibleEmptyTiles() {
+
+		final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+		for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
+			emptyTileMap.put(i, new EmptyTile(i));
+		}
+
+		// return ImmutableMap.copyOf(emptyTileMap);
+		return Collections.unmodifiableMap(emptyTileMap);
+		// return emptyTileMap;
 	}
-	
+
+	public static Tile createTile(final int tileCoordinate, final Piece piece) {
+		return piece != null ? new OccupiedTile(tileCoordinate, piece)
+				: EMPTY_TILES_CACHE.get(tileCoordinate);
+	}
+
 	public abstract boolean isTileOccupied();
-	
+
 	public abstract Piece getPiece();
-	
-	public static final class EmptyTile extends Tile{
-		EmptyTile( final int coordinate){
+
+	public static final class EmptyTile extends Tile {
+		EmptyTile(final int coordinate) {
 			super(coordinate);
 		}
-		
+
 		@Override
-		public boolean isTileOccupied(){
+		public boolean isTileOccupied() {
 			return false;
 		}
-		
+
 		@Override
-		public Piece getPiece(){
+		public Piece getPiece() {
 			return null;
 		}
 	}
-	
-	public static final class OccupiedTile extends Tile{
-		
+
+	public static final class OccupiedTile extends Tile {
+
 		private final Piece pieceOnTile;
-		
-		private OccupiedTile(int tileCoordinate,Piece pieceOnTile){
+
+		private OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
 			super(tileCoordinate);
-			this.pieceOnTile=pieceOnTile;
+			this.pieceOnTile = pieceOnTile;
 		}
-		
+
 		@Override
-		public boolean isTileOccupied(){
+		public boolean isTileOccupied() {
 			return true;
 		}
-		
+
 		@Override
-		public Piece getPiece(){
+		public Piece getPiece() {
 			return this.pieceOnTile;
 		}
 	}
